2b90332b3436a2673d696f4f7ed6ec5a6baa39b4
Dave Hocker <AtHomeX10@gmail.com>
2018-10-24 11:01:32 -0500

    Build documentation with executables

--- mpd-meson-configure.sh
+++ mpd-meson-configure.sh
@@ -6,6 +6,8 @@ meson . output/release --buildtype=debugoptimized -Db_ndebug=true
 # Disable upnp (how to alter configuration)
 # upnp depends on expat and meson cannot find it
 meson configure output/release -Dupnp=disabled
+# Try building documentation
+meson configure output/release -Ddocumentation=true
 
 # Verify unpnp is disabled (list current configuration settings)
 meson configure output/release

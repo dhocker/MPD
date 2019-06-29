47ce9faee3328ca1d3ad967ae954a5ec06ca0c6b
Dave Hocker <AtHomeX10@gmail.com>
2019-06-29 11:27:15 -0500

    Disable opus plugin because it won't build

--- mpd-meson-configure.sh
+++ mpd-meson-configure.sh
@@ -6,6 +6,8 @@ meson . output/release --buildtype=debugoptimized -Db_ndebug=true -Dupnp=disable
 # Disable upnp (how to alter configuration)
 # upnp depends on expat and meson cannot find it
 meson configure output/release -Dupnp=disabled
+# opus plugin fails to build
+meson configure output/release -Dopus=disabled
 # Try building documentation
 meson configure output/release -Ddocumentation=true
 
